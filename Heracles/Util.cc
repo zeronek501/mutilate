4501bf811d9e3213f36b2ae9100dccce453f914d
zeronek501 <zeronek501@gmail.com>
2018-09-08 03:35:18 +0900

    LLC alloc fixing

--- Heracles/Util.cc
+++ Heracles/Util.cc
@@ -9,6 +9,7 @@
 #include "Util.h"
 
 int s_write(std::string filepath, std::string value) {
+	/*
 	try {
 		std::cout << "filepath: " << filepath << " value: " << value << std::endl;
 		std::fstream fs;
@@ -19,26 +20,30 @@ int s_write(std::string filepath, std::string value) {
 	catch(std::ios_base::failure& e) {
 		std::cerr << "error: " << e.what() << std::endl;
 	}
+	*/
 
-/*
-	int fd = open(filepath.c_str(), O_WRONLY);
+	//int fd = open(filepath.c_str(), O_WRONLY);
+	FILE* fd = fopen(filepath.c_str(), "w");
 	printf("filepath: %s\n", filepath.c_str());
 	char *str;
 	//value.copy(str, value.length(), 0); // string copy
-	str = strdup(value.c_str());
+	//str = strdup(value.c_str());
 	if(fd == NULL) {
 		fprintf(stderr, "Unable to open path for writing\n");
 		return 1;
 	}
-	printf("string: %s\n", str);
-	fprintf(stdout, "before failure%d\n", errno);
+	//printf("string: %s\n", str);
+	//fprintf(stdout, "before failure%d\n", errno);
+	fprintf(fd, "%s", value.c_str());
+	/*
 	if(write(fd, str, value.length()) < 0) {
 		fprintf(stderr, "write failure%d\n", errno);
 		close(fd);
 		return -1;
 	}
-	close(fd);
-*/
+	*/
+	//close(fd);
+	fclose(fd);
 	return 0;
 }
 	
